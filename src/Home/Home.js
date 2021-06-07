99e41c70888c2eabde297318723d05baf60992bf
Rakesh Chandrasekaran <rakesh.chandrasekaran@mrcooper.com>
2021-06-07 22:46:15 +0530

    home page updated with react

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,23 +1,157 @@
 import React from 'react';
+import './Home.css';
 import L from '../utils/localization';
+import chennaiHub from '../images/home/Chennai Hub.png';
+import vector from '../images/common/Vector.png';
+import schoolAndUniversirt from '../images/home/schoolsanduniversity.png';
+import currentEventImage1 from '../images/home/current_event_image1.png';
+import currentEventImage2 from '../images/home/current_event_image2.png';
+import currentEventImage3 from '../images/home/current_event_image3.png';
+import students from '../images/home/students.png';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+
+
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
+
 
 const Home = () => (
-  <p>
-      <h3>{L.t('home.header')}</h3>
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  <div>
+    
+    <div className="col-xs-12 col-sm-12 backgroud-banners1">
+    <br/>
+    <br/>
+      <div className="caption">
+        <img src={chennaiHub} alt="" />
+      </div>
+      <div className="div-home">
+        <a className="navbar-navigation-button btn-home"  id="btnWorkWithUsInSIdeBanner" href="./workwithus.html">
+        {L.t('home.firstRowBtn')}
+        </a>
+      </div>
+    </div>
+    <div className="col-xs-12 col-sm-12 backgroud-banners2" >
+      <br />
+      <div className="main-caption2-heading">
+        <h1 id="hSecondBannerHeading" className="hSecondBannerHeading" >{L.t('home.secondBannerHeading')}</h1>
+        <img src={vector} alt="" className="vectorCSS" ></img>
+      </div>
+      <div id="divCarausal" className="col-xs-12 col-sm-12 carousal">
+        <Splide
+				options={ {
+					rewind : true,
+					perPage: 3,
+					perMove: 1,
+					gap    : '1rem',
+				} }>
+          <SplideSlide>
+            <img src={currentEventImage1} className="carousal-img-css" alt="Image 1"/>
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage2} className="carousal-img-css" alt="Image 2" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage3} className="carousal-img-css" alt="Image 2" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage1} className="carousal-img-css" alt="Image 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage2} className="carousal-img-css" alt="Image 2" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage3} className="carousal-img-css" alt="Image 2" />
+          </SplideSlide>
+        </Splide>
+      </div>
 
-    Why do we use it?
-    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      <div id="divCarausalMobile" className="col-xs-12 col-sm-12 carousal divCarausalMobile" style={{display: 'none'}}>
+        <Splide
+				options={ {
+					rewind : true,
+					perPage: 1,
+					perMove: 1,
+					gap    : '1rem',
+				} }>
+          <SplideSlide>
+            <img src={currentEventImage1} className="carousal-img-css" alt="Image 1"/>
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage2} className="carousal-img-css" alt="Image 2" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage3} className="carousal-img-css" alt="Image 2" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage1} className="carousal-img-css" alt="Image 1" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage2} className="carousal-img-css" alt="Image 2" />
+          </SplideSlide>
+          <SplideSlide>
+            <img src={currentEventImage3} className="carousal-img-css" alt="Image 2" />
+          </SplideSlide>
+        </Splide>
+      </div>
+    </div>
 
+    <div className="col-xs-12 col-sm-12 backgroud-banners3">
+      <br />
+      <div className="main-caption3-heading row">
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <h1 id="hThirdBannerHeading" className="hThirdBannerHeading">{L.t('home.thirdBannerHeading')}</h1>
+          <img src={vector} alt="" className="vectorCSS" ></img>
+        </div>
+        <div className="row" style={{ marginTop: '30px' }}>
+          <br />
+          <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          </div>
+          <div className="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+            <h5 id="hThirdBannerHeadingDescription" style={{
+              color: "#161515", fontSize: "16px",
+              textAlign: "center"
+            }}>{L.t('home.thirdBannerHeadingDescription')}</h5>
+          </div>
+          <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+          </div>
+        </div>
+        <div className="col-xs-12 col-sm-12" style={{ marginTop: '50px', paddingBottom: '10px' }}>
+          <h3 id="hThirdBannerSubHeading" style={{ fontSize: '26px', color: '#005A9E', fontSize: '26px', texAlign: 'center' }}>
+            {L.t('home.thirdBannerSubHeading')}
+          </h3>
+        </div>
+        <div className="col-xs-12 col-sm-12 row" style={{ textAlign: 'center' }}>
+          <div className="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+            <img style={{ width: '208px', height: '208px' }} src={students} />
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+            <img style={{ width: '208px', height: '208px' }} src={schoolAndUniversirt} />
+          </div>
+          <div className="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          </div>
+        </div>
 
-    Where does it come from?
-    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12" style={{ textAlign: 'center', marginTop: '50px' }}>
+          <button id="btnBeaChangeMaker" className="btn" style={{
+            textAlign: 'center', width: '341px', height: '66px',
+            background: '#004578', borderRadius: '3px', color: '#FFFFFF', fontSize: '22px', marginBottom: '25px'
+          }}>
+            {L.t('home.thirdBannerBeAChangeButton')}
+          </button>
+        </div>
 
-    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+      </div>
+    </div>
 
-    Where can I get some?
-    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-  </p>
+  </div>
 );
-
 export default Home;
+
+
+// options={{
+//   rewind: true,
+//   perPage: 3,
+//   perMove: 1
+// }}
--- src/utils/localization.js
+++ src/utils/localization.js
@@ -2,10 +2,51 @@ import _ from 'lodash';
 
 const englishContent = {
   home: {
-    header: 'Welcome'
+    firstRowBtn: 'Work With Us',
+    secondBannerHeading: 'Current Events',
+    Events: [{
+      'event_name': '',
+      'from_date': '',
+      'to_date': '',
+      'event_location': ''
+    }],
+    thirdBannerHeading: 'Changemaker’s Program',
+    thirdBannerHeadingDescription: 'Change begins at home and we want to be able to bring about grassroot level changes through our program. The Changemaker’s program exclusively focuses on creating academic engagements for C-Hub in the city.',
+    thirdBannerSubHeading: 'Who can join the Changemaker’s Program ?',
+    thirdBannerBeAChangeButton: 'Be a Change Maker'
   },
   aboutUs: {
-    header: 'About Us',
+    firstRowHeader: 'About Us',
+    firstRowSubHeade: [
+      'Innovate.',
+      'Sustain.',
+      'Enable.'
+    ],
+    firstRowParagraph: [
+      'C-Hub (Chennai-Hub) is the first of its kind innovation Hub set up by the Greater Chennai Corporation. This is an attempt to foster and promote innovation across different verticals. It harps on the need for Academia, Industries and GCC to work together to encourage good and innovative ideas.',
+      'A multidisciplinary team will be put together in order to approach problems through a holistic lens. The innovation hub will host stakeholders from different verticals to be part of this platform. It is an inclusive platform which will attempt to solve social, environmental and urban problems in the city together with its citizens.',
+      'A new paradigm is required that essentially brings urban stakeholders to collectively provide the know-how to urban challenges ensuring their expertise and perspective is reflected in urban solutioning for the future. A coming together of these critical stakeholders, termed as the Quadruple Helix, can aid in active flow of ideas and information to create both value and reduce risks, in essence, creating an ‘innovation value-network’ that spills benefits across the urban ecosystem gearing our cities for the future.'
+    ],
+    secondRowHeader: 'Core Functions',
+    secondRowData: [
+      'Enable and sustain the discovery and deployment of innovation to tackle urban challenges',
+      'Increasing economic growth',
+      'Enhance quality of life and ensure sustainability in processes'
+    ],
+    thirdRowHeader: 'Team',
+    fourthRowHeader: 'Mission',
+    fourthRowData: [
+      'A cross-boundary collaborative regime instituted to decide on innovation pathways.',
+      'Building partnerships between the quadruple helix (QH) of stakeholders into an ecosystem for know-how solutioning.'
+    ],
+    fifthRowHeader: 'Vision',
+    fifthRowData: [
+      'To become the largest think tank in the Country in the next three years.',
+      'To successfully establish Proof of concept by piloting in Chennai.',
+      'To create a working template that can be adapted by other cities in Tamil Nadu.',
+      'To be a platform for intersectionality of academics, entrepreneurship and the Chennai Corporation.'
+    ],
+    sixthRowHeader: 'Values'
   },
   subNav: {
     home: 'Home',
@@ -21,10 +62,51 @@ const englishContent = {
 
 const tamilContent = {
   home: {
-    header: 'வரவேற்பு'
+    firstRowBtn: 'எங்களுடன் வேலை செய்யுங்கள்',
+    secondBannerHeading: 'தற்போதைய நிகழ்வுகள்',
+    Events: [{
+      'event_name': '',
+      'from_date': '',
+      'to_date': '',
+      'event_location': ''
+    }],
+    thirdBannerHeading: 'ேஞ்ச்மேக்கரின் திட்டம்',
+    thirdBannerHeadingDescription: 'ாற்றம் வீட்டிலேயே தொடங்குகிறது, மேலும் எங்கள் திட்டத்தின் மூலம் அடிமட்ட அளவிலான மாற்றங்களைக் கொண்டு வர விரும்புகிறோம். சேஞ்ச்மேக்கரின் திட்டம் நகரத்தில் சி-ஹப்பிற்கான கல்வி ஈடுபாடுகளை உருவாக்குவதில் மட்டுமே கவனம் செலுத்துகிறது.',
+    thirdBannerSubHeading: 'சேஞ்ச்மேக்கரின் திட்டத்தில் யார் சேரலாம்?',
+    thirdBannerBeAChangeButton: 'ஒரு மாற்றத்தை உருவாக்குபவராக இருங்கள்'
   },
   aboutUs: {
-    header: 'சென்னை மாநகராட்சியைப் பற்றி',
+    firstRowHeader: 'எங்களை பற்றி',
+    firstRowSubHeade: [
+      'புதுமை.',
+      'நிலைத்திருங்கள்.',
+      'இயக்கு.'
+    ],
+    firstRowParagraph: [
+      'கிரேட்டர் சென்னை கார்ப்பரேஷனால் அமைக்கப்பட்ட சி-ஹப் (சென்னை-ஹப்) அதன் முதல் கண்டுபிடிப்பு மையமாகும். வெவ்வேறு செங்குத்துகளில் புதுமைகளை வளர்ப்பதற்கும் ஊக்குவிப்பதற்கும் இது ஒரு முயற்சி. நல்ல மற்றும் புதுமையான யோசனைகளை ஊக்குவிக்க அகாடெமியா, இண்டஸ்ட்ரீஸ் மற்றும் ஜி.சி.சி இணைந்து செயல்பட வேண்டியதன் அவசியத்தை இது விளக்குகிறது.',
+      'ஒரு முழுமையான லென்ஸ் மூலம் சிக்கல்களை அணுகுவதற்காக ஒரு பல்வகைக் குழு ஒன்று சேர்க்கப்படும். கண்டுபிடிப்பு மையம் இந்த தளத்தின் ஒரு பகுதியாக வெவ்வேறு செங்குத்துகளிலிருந்து பங்குதாரர்களை வழங்கும். இது ஒரு உள்ளடக்கிய தளமாகும், இது நகரத்தில் உள்ள சமூக, சுற்றுச்சூழல் மற்றும் நகர்ப்புற பிரச்சினைகளை அதன் குடிமக்களுடன் சேர்ந்து தீர்க்க முயற்சிக்கும்.',
+      'ஒரு புதிய முன்னுதாரணம் தேவைப்படுகிறது, இது நகர்ப்புற பங்குதாரர்களுக்கு அவர்களின் நிபுணத்துவத்தையும் முன்னோக்கையும் உறுதிப்படுத்தும் நகர்ப்புற சவால்களை எவ்வாறு கூட்டாக வழங்குவதற்கான எதிர்காலத்தை நகர்ப்புற தீர்வுகளில் பிரதிபலிக்கிறது. நான்கு முக்கியமான ஹெலிக்ஸ் என அழைக்கப்படும் இந்த முக்கியமான பங்குதாரர்களின் ஒன்றிணைவு, மதிப்பு மற்றும் அபாயங்களை இரண்டையும் உருவாக்குவதற்கான யோசனைகள் மற்றும் தகவல்களின் செயலில் ஓட்டத்திற்கு உதவக்கூடும், சாராம்சத்தில், நகர்ப்புற சுற்றுச்சூழல் அமைப்பு முழுவதும் நன்மைகளை பரப்பும் ஒரு புதுமை மதிப்பு-வலையமைப்பை உருவாக்குகிறது. எதிர்காலத்திற்கான எங்கள் நகரங்கள்.'
+    ],
+    secondRowHeader: 'முக்கிய செயல்பாடுகள்',
+    secondRowData: [
+      'நகர்ப்புற சவால்களை சமாளிக்க புதுமைகளின் கண்டுபிடிப்பு மற்றும் வரிசைப்படுத்தலை இயக்கவும் பராமரிக்கவும்',
+      'பொருளாதார வளர்ச்சியை அதிகரிக்கும்',
+      'வாழ்க்கைத் தரத்தை மேம்படுத்துதல் மற்றும் செயல்முறைகளில் நீடித்த தன்மையை உறுதி செய்தல்'
+    ],
+    thirdRowHeader: 'அணி',
+    fourthRowHeader: 'ிஷன்',
+    fourthRowData: [
+      'கண்டுபிடிப்பு பாதைகளை தீர்மானிக்க ஒரு குறுக்கு எல்லை கூட்டு ஆட்சி நிறுவப்பட்டது.',
+      'அறிவை எவ்வாறு தீர்ப்பதற்கான ஒரு சுற்றுச்சூழல் அமைப்பில் பங்குதாரர்களின் நான்கு மடங்கு ஹெலிக்ஸ் (கியூஎச்) இடையே கூட்டாண்மைகளை உருவாக்குதல்.'
+    ],
+    fifthRowHeader: 'ார்வை',
+    fifthRowData: [
+      'அடுத்த மூன்று ஆண்டுகளில் நாட்டின் மிகப்பெரிய சிந்தனைக் குழுவாக மாறுவது.',
+      'சென்னையில் பைலட் செய்வதன் மூலம் கருத்து ஆதாரத்தை வெற்றிகரமாக நிறுவுதல்.',
+      'தமிழ்நாட்டின் பிற நகரங்களால் மாற்றியமைக்கக்கூடிய ஒரு வேலை வார்ப்புருவை உருவாக்க. சென்னையில் பைலட்டிங்.',
+      'கல்வியாளர்கள், தொழில்முனைவோர் மற்றும் சென்னை கார்ப்பரேஷன் ஆகியவற்றின் குறுக்குவெட்டுக்கான தளமாக இருக்க வேண்டும்.'
+    ],
+    sixthRowHeader: 'மதிப்புகள்'
   },
   subNav: {
     home: 'முகப்பு',
@@ -40,7 +122,7 @@ const tamilContent = {
 
 
 const translate = (contentPath, values) => {
-  const language = window.sessionStorage.getItem("language");
+  const language = window.sessionStorage.getItem('language');
   const contentToBePicked = language === 'tamil' ? tamilContent : englishContent;
   const content = _.get(contentToBePicked, contentPath, null);
   if (content instanceof Array) {
